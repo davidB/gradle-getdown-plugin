8cb36a8620c41fb32e006d52ca568b24de3dd7c4
David Bernard <david.bernard.31@gmail.com>
2014-09-25 12:55:39 +0200

    disable MAC from Platform

--- src/main/groovy/bundles/Platform.java
+++ src/main/groovy/bundles/Platform.java
@@ -3,7 +3,7 @@ package bundles;
 public enum Platform implements java.io.Serializable {
 	WINDOWS_X64("windows-x64", "windows-x86_64", "windows-x64", "windows-amd64")
 	, WINDOWS_I586("windows-i586", "windows-i386", "windows-x86", "windows")
-	, MACOSX_X64("macosx-x64", "osx", "mac os x", "darwin")
+	//, MACOSX_X64("macosx-x64", "osx", "mac os x", "darwin")
 	, LINUX_X64("linux-x64", "linux-amd64", "linux-x86_64")
 	, LINUX_I586("linux-i586", "linux-i386", "linux-x86")
 	;

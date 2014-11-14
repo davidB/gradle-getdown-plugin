2ae3899ca0304cc642156af61318f6521a8cced5
David Bernard <david.bernard.31@gmail.com>
2014-11-15 00:55:02 +0100

    refactor of GetJreTask
    - fix some gradle cache issue  due to @Input + Custom type (should fix
    #6)
    - keep download tar.gz for debug, avoid re-download
    - add logger.debug
    - use ant.move (vs File.rename) to failed if issue and try several way
    to rename (including File.rename)

--- src/main/groovy/bundles/GetJreTask.groovy
+++ src/main/groovy/bundles/GetJreTask.groovy
@@ -10,10 +10,10 @@ class GetJreTask extends DefaultTask {
 	@OutputDirectory
 	def File dir
 
-	@Input
+	//@Input
 	def Platform[] platforms
 
-	@Input
+	//@Input
 	def JreVersion version
 
 	def cachePath(Platform platform) {
@@ -22,25 +22,29 @@ class GetJreTask extends DefaultTask {
 
 	@TaskAction
 	def updateDir() {
-		jres.addAll(platforms.collect {
-			File cache = cachePath(it)
+		jres.addAll(platforms.collect { jre ->
+			File cache = cachePath(jre)
 			if (!cache.exists()) {
 				logger.info("creating : " + cache);
 				File tmp = new File(cache.toString() + ".tmp")
 				tmp.mkdirs()
+				File downloaded = new File(tmp, "jre.tar.gz")
+				if (!downloaded.exists()) {
+					URL src = JreTools.toOracleDownloadUrl(version, jre)
+					logger.info("download : " + src+ " -> " + downloaded)
+					JreTools.downloadJre(src, downloaded)
+				}
 				File jreDir = new File(tmp, JreTools.findJreDirNameInJar(version))
-				if (!jreDir.exists()) {
-					File downloaded = new File(tmp, "jre.tar.gz")
-					if (!downloaded.exists()) {
-						URL src = JreTools.toOracleDownloadUrl(version, it)
-						logger.info("download : " + src);
-						JreTools.downloadJre(src, downloaded)
-					}
+				if (!jreDir.exists() || jreDir.listFiles().size == 0) {
+					logger.debug("untar")
 					ant.untar(src: downloaded, compression : 'gzip', dest: tmp)
-					ant.delete(file: downloaded)
-					new File(tmp, JreTools.findJreDirNameInTar(version)).renameTo(jreDir)
+					logger.debug("rename : " + new File(tmp, JreTools.findJreDirNameInTar(version)) + " to " + jreDir)
+					//new File(tmp, JreTools.findJreDirNameInTar(version)).renameTo(jreDir)
+					ant.move(file: new File(tmp, JreTools.findJreDirNameInTar(version)), tofile: jreDir)
 				}
 				//see https://blogs.oracle.com/jtc/entry/reducing_your_java_se_runtime
+				//TODO use a configuration file
+				//TODO support removing file pattern into .jar (like rt.jar)
 				ant.delete(){
 					fileset(dir: jreDir, includes:"man plugin lib/desktop lib/deploy lib/charsets.jar lib/ext/sunjce_provider.jar lib/ext/localedata.jar lib/ext/ldapsec.jar lib/ext/dnsns.jar bin/rmid bin/rmiregistry bin/tnameserv bin/keytool bin/kinit bin/klist bin/ktab bin/policytool bin/orbd bin/servertool bin/javaws lib/javaws/ lib/javaws.jar")
 				}

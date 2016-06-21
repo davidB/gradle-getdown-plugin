94b04bc1d3e412474ab47429fa16baf2ab19d0b7
Andre Busche <andre.busche@gmx.de>
2016-06-21 15:37:21 +0200

    Auto-detect and use HTTP-Proxy
    Check whether both system properties http.proxyPort and http.proxyHost are configured. If so, configure a HTTP Proxy to download a JRE.

--- src/main/groovy/bundles/JreTools.groovy
+++ src/main/groovy/bundles/JreTools.groovy
@@ -3,6 +3,8 @@ package bundles
 import com.squareup.okhttp.OkHttpClient
 import com.squareup.okhttp.Request
 
+import java.net.Proxy;
+
 //TODO add testcase
 public class JreTools {
 	public static JreVersion current() {
@@ -37,12 +39,31 @@ public class JreTools {
 		"java_vm"
 	}
 
+	private static def isHttpProxyConfigured() {
+		return null != System.getProperty("http.proxyHost") &&
+			null != System.getProperty("http.proxyPort");
+	}
+	
 	//see https://ivan-site.com/2012/05/download-oracle-java-jre-jdk-using-a-script/
 	//TODO run async download
 	public static def downloadJre(URL src, File dest) {
 		//ant.get(src: cfg.jvmVersion.toOracleDownloadUrl(it), dest: downloaded, httpusecaches: false, skipexisting: true)
 		//URLConnection uc = src.openConnection();
 		def client = new OkHttpClient();
+		if (isHttpProxyConfigured()) {
+			int proxyPort=-1;
+			try {
+				proxyPort = Integer.valueOf(System.getProperty("http.proxyPort"))
+			} catch (NumberFormatException e) {
+				// number could not be parsed as a number.
+			}
+			if (proxyPort>=0) {
+				def proxyHost = System.getProperty("http.proxyHost")
+				def proxyAddress = InetSocketAddress.createUnresolved(proxyHost, proxyPort)
+				Proxy proxy  = new Proxy(Proxy.Type.HTTP, proxyAddress)
+				client = client.setProxy(proxy);
+			}
+		}
 		def request = new Request.Builder().url(src)
 			.addHeader("Cookie", "oraclelicense=accept-securebackup-cookie")
 			//.addHeader("Content-Type", "application/")
